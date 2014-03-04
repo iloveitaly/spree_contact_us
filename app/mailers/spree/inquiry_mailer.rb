3c9e0de7dbb1817179ba914696a5ccf82637b253
Michael Bianco <iloveitaly@gmail.com>
2014-03-04 17:04:41 -0500

    Stop using intercept email as recipient
    The preference started having an empty string
    which then made the to field blank

--- app/mailers/spree/inquiry_mailer.rb
+++ app/mailers/spree/inquiry_mailer.rb
@@ -5,7 +5,7 @@ module Spree
     def notification(inquiry, customer = false)
       @inquiry = inquiry
 
-      recipients = Spree::MailMethod.current.preferred_intercept_email || Spree::MailMethod.current.preferred_mail_bcc.split
+      recipients = Spree::MailMethod.current.preferred_mail_bcc.split
 
       # rather use reply-to for spam reasons (the email really isn't coming from the user)
       # but it's not possible: http://productforums.google.com/forum/#!topic/gmail/azLFdnPfRZo
